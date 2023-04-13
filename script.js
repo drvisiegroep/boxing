b78aae855451f790e34bdb1010203cccd728525a
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2023-04-13 11:15:03 +0200

    btn

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const comboNumber = document.querySelector('.combo');
+const startcomboBtn = document.querySelector('.startcombo-btn');
 
 const timedMovesets = {
-  '1':1500 ,
+  '1':1500,
   '2':2000,
   '3':3000,
   '4':4000,
   '3 hooks':3000,
   '4 hooks':4000,
-  '5 hooks':5000,
+  //'5 hooks':5000,
   'left hook, right straight': 2000,
   'right straight, left hook': 2000,
   'right uppercut':1500,
   'left uppercut':1500,
+  'left hook': 1500,
+  'right hook': 1500,
   'body shot':1500,
   'liver shot':1500,
 }
@@ -34,7 +37,8 @@ function newMove() {
 }
 
 //Start
-newMove();
+startcomboBtn.addEventListener('click', newMove())
+
 
 
 
